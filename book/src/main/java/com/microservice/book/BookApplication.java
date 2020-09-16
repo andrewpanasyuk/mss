c15ddd464a6946f0b48d70c833776c20fc86ad0e
Andrew Panasyuk <a.panasyuk@a2b.direct>
2020-09-16 17:23:34 +0300

    add eureka

--- book/src/main/java/com/microservice/book/BookApplication.java
+++ book/src/main/java/com/microservice/book/BookApplication.java
@@ -2,8 +2,11 @@ package com.microservice.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
